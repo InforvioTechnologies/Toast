af2f513b587507625326d7791b9cd79af187d425
mathayan <arivindh321@gmail.com>
2021-08-09 15:26:58 +0530

    Initial Commit

---
+++ ToasterLibrary/src/main/java/in/loanwiser/toasterlibrary/ToasterMessage.java
@@ -0,0 +1,12 @@
+package in.loanwiser.toasterlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+class ToasterMessage {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
